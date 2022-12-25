1fb16d3c80646787eae01c79d15730867e721598
Nicolas Thery <nthery@gmail.com>
2022-12-25 19:09:57 +0100

    day 1: update for part 2

--- src/day1.rs
+++ src/day1.rs
@@ -1,4 +1,3 @@
-use std::cmp::max;
 use std::env;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
@@ -14,26 +13,26 @@ fn main() -> anyhow::Result<()> {
     let file = File::open(&args[1]).context(format!("cannot open {}", args[1]))?;
     let mut file_reader = BufReader::new(file);
 
-    let nb_calories = parse_total_food(&mut file_reader)?;
+    let nb_calories = parse_top_3_packs(&mut file_reader)?;
     println!("Biggest pack: {} calories", nb_calories);
 
     Ok(())
 }
 
-fn parse_total_food<T: BufRead>(reader: &mut T) -> anyhow::Result<u32> {
-    let mut biggest_meal = 0u32;
+fn parse_top_3_packs<T: BufRead>(reader: &mut T) -> anyhow::Result<u32> {
+    let mut packs = vec![];
     loop {
-        let nb_calories = parse_elf_food(reader)?;
+        let nb_calories = parse_elf_pack(reader)?;
         if nb_calories == 0 {
             break;
         }
-        biggest_meal = max(biggest_meal, nb_calories);
+        packs.push(nb_calories);
     }
-
-    Ok(biggest_meal)
+    packs.sort_by(|l, r| r.cmp(l));
+    Ok(packs.iter().take(3).sum())
 }
 
-fn parse_elf_food<T: BufRead>(reader: &mut T) -> anyhow::Result<u32> {
+fn parse_elf_pack<T: BufRead>(reader: &mut T) -> anyhow::Result<u32> {
     let mut total = 0u32;
     for line in reader.lines() {
         let line = line?;
