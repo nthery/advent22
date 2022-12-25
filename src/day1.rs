3c4f6010a765b4fcd02a80768812c8a3fc0643c8
Nicolas Thery <nthery@gmail.com>
2022-12-25 20:30:15 +0100

    day 1: use iterator

--- src/day1.rs
+++ src/day1.rs
@@ -1,6 +1,6 @@
 use std::env;
 use std::fs::File;
-use std::io::{BufRead, BufReader};
+use std::io::{BufRead, BufReader, Lines};
 
 use anyhow::Context;
 
@@ -19,29 +19,48 @@ fn main() -> anyhow::Result<()> {
     Ok(())
 }
 
+// Return the total number of calories in the the first 3 backpacks by descending order.
 fn parse_top_3_packs<T: BufRead>(reader: &mut T) -> anyhow::Result<u32> {
-    let mut packs = vec![];
-    loop {
-        let nb_calories = parse_elf_pack(reader)?;
-        if nb_calories == 0 {
-            break;
-        }
-        packs.push(nb_calories);
-    }
-    packs.sort_by(|l, r| r.cmp(l));
-    Ok(packs.iter().take(3).sum())
+    let packs = packs(reader);
+    let mut calories = packs.collect::<anyhow::Result<Vec<u32>>>()?;
+    calories.sort_by(|l, r| r.cmp(l));
+    Ok(calories.iter().take(3).sum())
 }
 
-fn parse_elf_pack<T: BufRead>(reader: &mut T) -> anyhow::Result<u32> {
-    let mut total = 0u32;
-    for line in reader.lines() {
-        let line = line?;
-        if line.is_empty() {
-            break;
-        }
-        let nb_calories: u32 = line.parse()?;
-        total += nb_calories;
+// Return an iterator over the calories contained in the backpacks of each elf.
+fn packs<B: BufRead>(reader: B) -> Packs<B> {
+    Packs {
+        lines: reader.lines(),
     }
+}
 
-    Ok(total)
+struct Packs<B: BufRead> {
+    lines: Lines<B>,
+}
+
+impl<B: BufRead> Iterator for Packs<B> {
+    type Item = Result<u32, anyhow::Error>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let mut total = 0u32;
+        for maybe_line in &mut self.lines {
+            let line = match maybe_line {
+                Ok(payload) => payload,
+                Err(e) => return Some(Err(e.into())),
+            };
+            if line.is_empty() {
+                break;
+            }
+            match line.parse::<u32>() {
+                Ok(n) => total += n,
+                Err(e) => return Some(Err(e.into())),
+            }
+        }
+
+        if total > 0 {
+            Some(Ok(total))
+        } else {
+            None
+        }
+    }
 }
